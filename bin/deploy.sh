958ffb58bd744be838d9be59df383a4748003428
kjake <kjake@users.noreply.github.com>
2018-01-04 10:01:40 -0500

    Variablize this too

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-cd /usr/local/share/java/unifi
-openssl pkcs12 -export -in cert/[FQDN]/fullchain.pem -inkey cert/[FQDN]/privkey.pem -out cert/[FQDN]/signed.p12 -name unifi -password pass:aircontrolenterprise
-keytool -importkeystore -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore data/keystore -srckeystore cert/[FQDN]/signed.p12 -srcstoretype PKCS12 -srcstorepass aircontrolenterprise -alias unifi -noprompt
-service unifi restart
+UNIFI=/usr/local/share/java/unifi
+CERTS=/etc/dehydrated/certs
+FQDN=unifi.example.com
+
+if [ ! -e ${CERTS}/${FQDN}/fullchain.pem ]; then dehydrated -c; fi
+if [ -z ${CERTS}/${FQDN}/fullchain.pem ]; then exit 1; fi
+
+openssl pkcs12 -export -in ${CERTS}/${FQDN}/fullchain.pem -inkey ${CERTS}/${FQDN}/privkey.pem -out ${CERTS}/${FQDN}/signed.p12 -name ${JAIL_NAME} -password pass:aircontrolenterprise
+keytool -importkeystore -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore ${UNIFI}/data/keystore -srckeystore ${CERTS}/${FQDN}/signed.p12 -srcstoretype PKCS12 -srcstorepass aircontrolenterprise -alias ${JAIL_NAME} -noprompt
+service ${JAIL_NAME} restart

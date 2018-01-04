642cdacc834fe479d7cb00d46af979f827dd1a75
kjake <kjake@users.noreply.github.com>
2018-01-03 23:13:18 -0500

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p /etc/dehydrated/accounts
-"if [ ! -z /etc/dehydrated/config ]; then cp /usr/local/etc/dehydrated/config.example /etc/dehydrated/config; fi"
-"if [ ! -z /etc/dehydrated/domains.txt ]; then cp /usr/local/etc/dehydrated/domains.txt.example /etc/dehydrated/domains.txt; fi"
-"mkdir /opt && cd /opt && git clone https://github.com/kappataumu/letsencrypt-cloudflare-hook && pip install -r letsencrypt-cloudflare-hook/requirements-python-2.txt"
+if [ ! -z /etc/dehydrated/config ]; then cp /usr/local/etc/dehydrated/config.example /etc/dehydrated/config; fi
+if [ ! -z /etc/dehydrated/domains.txt ]; then cp /usr/local/etc/dehydrated/domains.txt.example /etc/dehydrated/domains.txt; fi
+mkdir /opt && cd /opt && git clone https://github.com/kappataumu/letsencrypt-cloudflare-hook && pip install -r letsencrypt-cloudflare-hook/requirements-python-2.txt
 dehydrated --register --accept-terms

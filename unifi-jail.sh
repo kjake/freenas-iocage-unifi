926be2459932d33b831490bd6e0d76412a1e6e21
kjake <kjake@users.noreply.github.com>
2018-01-04 11:55:19 -0500

    Update unifi-jail.sh

--- unifi-jail.sh
+++ unifi-jail.sh
@@ -12,9 +12,9 @@ rm /tmp/pkg.json
 iocage fstab -a ${JAIL_NAME} linproc /proc linprocfs rw 0 0
 iocage fstab -a ${JAIL_NAME} ${JAIL_PATH}/portsnap/ports /usr/ports nullfs rw 0 0
 iocage fstab -a ${JAIL_NAME} ${JAIL_PATH}/portsnap/db /var/db/portsnap nullfs rw 0 0
+iocage fstab -a ${JAIL_NAME} ${JAIL_PATH}/${JAIL_NAME}/dehydrated /etc/dehydrated nullfs rw 0 0
 iocage fstab -a ${JAIL_NAME} ${JAIL_PATH}/${JAIL_NAME}/data /usr/local/share/java/unifi/data nullfs rw 0 0
 iocage fstab -a ${JAIL_NAME} ${JAIL_PATH}/${JAIL_NAME}/logs /usr/local/share/java/unifi/logs nullfs rw 0 0
-iocage fstab -a ${JAIL_NAME} ${JAIL_PATH}/${JAIL_NAME}/dehydrated /etc/dehydrated nullfs rw 0 0
 iocage exec ${JAIL_NAME} "if [ -z /usr/ports ]; then portsnap fetch extract; else portsnap auto; fi"
 iocage exec ${JAIL_NAME} make -C /usr/ports/net-mgmt/unifi5 clean install BATCH=yes
 iocage exec ${JAIL_NAME} sh /etc/dehydrated/install.sh
